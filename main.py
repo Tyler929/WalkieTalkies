b3f28ef3f22e0d24dd5d0b32761172e147542532
katiehickman <katie.hickman2704@gmail.com>
2021-12-01 15:57:27 -0800

    katie about me page and fixed navbar dropdown!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 
 # create a Flask instance
 app = Flask(__name__)
@@ -34,6 +35,20 @@ def stub():
 @app.route('/greet/')
 def greet():
     return render_template("greet.html")
+
+@app.route('/katie', methods=['GET', 'POST'])
+def katie():
+    url = "https://numbersapi.p.rapidapi.com/random/trivia"
+
+    querystring = {"min":"1","max":"100","fragment":"true","json":"true"}
+
+    headers = {
+        'x-rapidapi-host': "numbersapi.p.rapidapi.com",
+        'x-rapidapi-key': "fae3fd2dd7mshb51665dc058f9ecp12ba4djsn73ae03643dfa"
+    }
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    return render_template("katie.html", numbers=response.json())
+    print(response.text)
 
 # runs the application on the development server
 if __name__ == "__main__":
